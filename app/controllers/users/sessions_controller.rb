58fb37c351f3a9b7338fd033cb1955cc5a512c2f
ura0328 <ura20250103@gmail.com>
2020-04-16 05:23:33 +0000

    devise sign_in adjust

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,14 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    homes_top_path
+  end
+
+  def after_inactive_sign_in_path_for(resource)
+    homes_top_path
+  end
+  
   # GET /resource/sign_in
   # def new
   #   super
